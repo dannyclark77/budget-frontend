fc920e36c749d5570c6ece5397ec9bfb318c3666
dannyclark77 <dannyclark77@gmail.com>
2018-02-10 17:29:58 -0500

    Add content to budget component
    - Add modal to enter new budget categories
    - Add title and column headers

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-budget',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetComponent implements OnInit {
 
-  constructor() { }
+  modalRef: NgbModalRef;
+  budgetform: FormGroup;
+
+  constructor(
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit() {
+    this.budgetform = new FormGroup({
+      name: new FormControl(),
+      amount: new FormControl()
+    })
+  }
+
+  onNewCategory() {
+    console.log('form submitted', this.budgetform.value);
+    this.budgetform.reset();
+    this.modalRef.close();
+  }
+
+  open(content) {
+    console.log('open modal', content);
+    this.modalRef = this.modalService.open(content);
   }
 
 }
